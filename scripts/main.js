81510a86edd91cc57f952ff0e0ec30dabf8a7625
Mohened Dev <mohenedfouad@gmail.com>
2025-07-25 12:33:52 +0300

    Add more animation, create footer, and program a random game button.

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+//random game
+let gamesLinks = ["./pages/XO.html", "./pages/aimTest.html", "./pages/memoryGame.html", "./pages/hangMan.html"];
+let randomGameBtn = document.querySelector(".randomGameBtn");
+randomGameBtn.addEventListener("click", function() {
+    setTimeout(() => window.location.href = gamesLinks[Math.floor(Math.random() * gamesLinks.length)],500);
+});
+
+//deta set
+let deta = document.querySelector("#deta");
+let year = new Date().getFullYear();
+deta.innerText = year;

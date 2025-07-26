5c6d6378eacba43d8f063f9aa2d4c0467fd63491
Mohened Dev <mohenedfouad@gmail.com>
2025-07-26 16:35:26 +0300

    Finish the memory game

--- scripts/memoryGame.js
+++ scripts/memoryGame.js
@@ -1,76 +1,108 @@
-//start game
+"use strict";
+// Start game
+let started = false;
 document.querySelector(".startGameBtn").addEventListener("click", function () {
     this.parentElement.remove();
+    started = true;
 });
+
 let points = 0;
-
-//if flipibg card equals
-let cardOne;
-let cardTow;
-let equals = false;
-let howFliping = 2;
+let wrongPoints = 0;
+let wrong = document.querySelector("#wrong");
+let score = document.querySelector("#score");
+let cards = [];
+let canFliping = true;
 let found = [];
-function checking(card) {
-    if (cardOne === cardTow) {
-        equals = true;
-        points += 5;
-    }
-    else {
-        flipCancel(card);
-    }
-}
 
-//click cards and flip
+// Flip card logic
 document.querySelector(".gameBoard").onclick = (event) => {
-    let targ = event.target.parentElement;
-    console.log(targ);
+    let targ = event.target.closest(".card");
+
     if (
+        targ &&
         targ.classList.contains("card") &&
         !targ.classList.contains("active") &&
-        howFliping > 0
+        canFliping
     ) {
         flipCard(targ);
-        if (howFliping === 1) cardTow = targ.dataset.tech;
-        else cardOne = targ.dataset.tech;
-        console.log(`1 ${cardOne}`);
-        console.log(`2 ${cardTow}`);
     }
 };
+
 function flipCard(card) {
-    console.log(howFliping);
-    howFliping--;
-    checking(card)
     card.classList.add("active");
-    setTimeout(() => {
-        if (equals) {
-            equals = false;
-            found.push(card.dataset.tech);
-            howFliping = 2;
-        }
-        else {
-            if (!found.includes(card.dataset.tech))
-            flipCancel(card);
-        }
-    }, 1750);
-}
-function flipCancel(card) {
-    card.classList.remove("active");
-    howFliping++;
+    cards.push(card);
+
+    if (cards.length === 2) {
+        canFliping = false;
+
+        setTimeout(() => {
+            if (cards[0].dataset.tech === cards[1].dataset.tech) {
+                found.push(cards[0].dataset.tech);
+                points += 5;
+                score.innerText = points;
+                if (points === 45) win();
+            } else {
+                wrongPoints++;
+                wrong.innerText = wrongPoints;
+                cards.forEach(flipCancel);
+            }
+
+            cards = [];
+            canFliping = true;
+        }, 1000);
+    }
 }
 
-//random game
+function flipCancel(card) {
+    card.classList.remove("active");
+}
+
+//Timer and win
+let timerSpan = document.querySelector("#timer");
+let timer = 0;
+setInterval(() => {
+    if (started) {
+        timer++;
+        timerSpan.innerText = timer;
+        if (timer > 119) lost();
+    }
+}, 1000);
+
+function win() {
+    let winScrean = `
+    <div class="startButtonCont">
+        <p>your time is: ${timer}S</p>
+        <button class="startGameBtn" onclick="location.reload()">restart Game</button>
+    </div>
+    `;
+    document.querySelector("#gameContainer").innerHTML += winScrean;
+    started = false;
+}
+
+// Lost
+function lost() {
+    let lostScrean = `
+    <div class="startButtonCont">
+        <p>You Lost</p>
+        <button class="startGameBtn" onclick="location.reload()">restart Game</button>
+    </div>
+    `;
+    document.querySelector("#gameContainer").innerHTML += lostScrean;
+    started = false;
+    canFliping = false;
+}
+
+// Random game
 let gamesLinks = ["./XO.html", "./aimTest.html", "./hangMan.html"];
 let randomGameBtn = document.querySelector(".randomGameBtn");
 randomGameBtn.addEventListener("click", function () {
-    setTimeout(
-        () =>
-            (window.location.href =
-                gamesLinks[Math.floor(Math.random() * gamesLinks.length)]),
-        500
-    );
+    setTimeout(() => {
+        window.location.href =
+            gamesLinks[Math.floor(Math.random() * gamesLinks.length)];
+    }, 500);
 });
 
-//deta set
+// Footer year
 let deta = document.querySelector("#deta");
 let year = new Date().getFullYear();
 deta.innerText = year;
